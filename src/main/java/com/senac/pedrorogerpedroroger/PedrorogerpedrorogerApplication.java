cf6ce620a7ef913e43782390c3c25213b9328657
rinnaide <phencostarj@gmail.com>
2025-10-25 18:49:35 -0300

    initial commit

---
+++ src/main/java/com/senac/pedrorogerpedroroger/PedrorogerpedrorogerApplication.java
@@ -0,0 +1,13 @@
+package com.senac.pedrorogerpedroroger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedrorogerpedrorogerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedrorogerpedrorogerApplication.class, args);
+	}
+
+}
